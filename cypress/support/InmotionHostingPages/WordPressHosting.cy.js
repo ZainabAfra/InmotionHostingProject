3748bd9b7bf6044196b118bc7ebc43a079696520
Afsar <Afsarfk012@gmail.com>
2024-06-13 15:54:22 -0400

    Added new features on test cases

--- cypress/support/InmotionHostingPages/WordPressHosting.cy.js
+++ cypress/support/InmotionHostingPages/WordPressHosting.cy.js
@@ -9,7 +9,29 @@ export class WordPressHosting
     continusBtn = "#sidebar-next-step"
     DomainName=":nth-child(3) > .required"
     continueBtn2="#sidebar-next-step"
-    
+    WPLaunch='//*[@id="wordpress-hosting-rostrum"]/div/div[2]/div[2]/div/div[2]/button'
+    DataCenterWPLaunch="//input[@value='1']"
+    DataCenterWPLaunch2="//input[@value='2']"
+    DataCenterWPLaunch3="//input[@value='3']"
+    WPLaunchContinueBtn="(//input[@id='sidebar-next-step'])[1]"
+    DomainName="(//label[@class='required radio new_domain'])[3]"
+    WPLaunchContinueBtn2="//input[@id='sidebar-next-step']"
+    WPPower='//*[@id="wordpress-hosting-rostrum"]/div/div[2]/div[2]/div/div[2]/button'
+    DataCenterWPPower="//input[@value='1']"
+    DataCenterWPPower2="//input[@value='2']"
+    DataCenterWPPower3="//input[@value='3']"
+    WPPowerContinueBtn="#sidebar-next-step"
+    DomainName=":nth-child(3) > .required"
+    WPPowerContinueBtn2="#sidebar-next-step"
+    WPHostingFeatures=".col-md-4.col-xs-6.col-sm-6.col-lg-4"
+
+
+
+
+
+
+
+
 
     wordPressDropdown(){cy.get(this.WordPressTab).invoke('show')}
     wordPressOncPanel(){cy.contains('WordPress on cPanel').click({force: true})}
@@ -18,6 +40,8 @@ export class WordPressHosting
         .and('have.length','4').each(($WPH, index, $list)=>{
             cy.log($WPH.text())
     })}
+
+    // WP Core
     selectWPCore(){cy.xpath(this.WPCore).click()
         cy.wait(3000)
     }
@@ -26,7 +50,31 @@ export class WordPressHosting
     verifyDataCenter3(){cy.xpath(this.DataCenter3).should('not.be.checked')}
     clickContinusBtn(){cy.get(this.continusBtn).click()}
     clickDomainName(){cy.get(this.DomainName).should('contain','I will decide later').click()}
-    clickContinusBtn2(){cy.get(this.continueBtn2).click()}
-    
-    
+    clickContinusBtn2(){cy.get(this.continueBtn2).click({force: true}).go(-3)}
+
+    //WP Launch
+    selectWPLaunch(){cy.xpath(this.WPLaunch).click()
+        cy.wait(3000)
+    }
+    verifyDataCenterWPLaunch(){cy.xpath(this.DataCenterWPLaunch).should('be.checked')}
+    verifyDataCenterWPLaunch2(){cy.xpath(this.DataCenterWPLaunch2).should('not.be.checked')}
+    verifyDataCenterWPLaunch3(){cy.xpath(this.DataCenterWPLaunch3).should('not.be.checked')}
+    clickWPLaunchContiunueBtn(){cy.xpath(this.WPLaunchContinueBtn).should('be.enabled').click({ force: true })}
+    clickDomainName(){cy.xpath(this.DomainName).click({force: true})}
+    clickWPLaunchContiunueBtn2(){cy.xpath(this.WPLaunchContinueBtn2).click({force: true}).go(-3)}
+
+    //WP Power
+    selectWPPower(){cy.xpath(this.WPPower).click()
+        cy.wait(3000)
+    }
+    verifyDataCenterWPPower(){cy.xpath(this.DataCenterWPPower).should('be.checked')}
+    verifyDataCenterWPPower2(){cy.xpath(this.DataCenterWPPower2).should('not.be.checked')}
+    verifyDataCenterWPPower3(){cy.xpath(this.DataCenterWPPower3).should('not.be.checked')}
+    clickWPPowerContiunueBtn(){cy.get(this.WPPowerContinueBtn).should('be.enabled').click({force: true})}
+    clickDomainName(){cy.get(this.DomainName).click({force: true})}
+    clickWPPowerContiunueBtn2(){cy.get(this.WPPowerContinueBtn2).should('be.enabled').click({force: true}).go(-3)}
+    verifyWPHostingFeatures(){cy.get(this.WPHostingFeatures).should('contain.text','WordPress Made Easy')
+        .and('have.length','9').each(($WPHF , index, $list)=>{
+            cy.log($WPHF.text())
+    })}
 }
